827d1cde29e789975d4422fcdf3c0e92b5eabba7
Nguyễn Khánh Ninh <ninhnk@arrowhitech.biz>
2022-09-23 16:01:10 +0700

    first commit

---
+++ app/src/main/java/com/aptech/team5/npfood/MainActivity.java
@@ -0,0 +1,14 @@
+package com.aptech.team5.npfood;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
